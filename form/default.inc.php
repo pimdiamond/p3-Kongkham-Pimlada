8ea113ee010cdbb56d83b13c170d3d87f7595a44
Pimlada <pimdiamond@gmail.com>
2020-12-18 07:25:20 -0500

    added form folder

---
+++ form/default.inc.php
@@ -0,0 +1,2 @@
+<h1>Sorry</h1>
+<p>Something went wrong please try again later!.</p>
---
+++ form/thankyou.inc.php
@@ -0,0 +1,3 @@
+<h1>Thank you for your enquiry</h1>
+<p>Thanks for your interest. we have received your message, and we will be in touch shortly.</p>
+
